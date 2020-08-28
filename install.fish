e7c0b9edb182ff90e399b1299cdec9a5f501aab3
Ishan Goel <igoel.mail@gmail.com>
2020-08-28 17:13:38 +0400

    Create install.fish

---
+++ install.fish
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/quackduck/WarpDrive/master/wd.fish > ~/.config/fish/functions/wd.fish # downloads the newest fish function file
+curl https://raw.githubusercontent.com/quackduck/WarpDrive/master/src/WarpDrive.java > ~/WarpDrive.java # downloads the newest java source code file
+cd ~ && javac WarpDrive.java && rm WarpDrive.java && cd - # compiles the java file and then deletes it
