0f3b319d056d808e6b7e18f510401d31d3d2e242
Ishan Goel <igoel.mail@gmail.com>
2020-09-02 18:22:39 +0400

    Added GitHub Action for testing WarpDrive

--- install.fish
+++ install.fish
@@ -1,7 +1,20 @@
 if test ! "$wd_source_containing_dir"
     set wd_source_containing_dir ~
 end
+mkdir -p ~/.config/fish/functions
+if test ! -e ~/.config/fish/functions/fish_prompt.fish
+  curl -sS https://raw.githubusercontent.com/quackduck/WarpDrive/master/example_fish_prompt.fish > ~/.config/fish/functions/fish_prompt.fish # downloads the newest fish function file
+end
 curl -sS https://raw.githubusercontent.com/quackduck/WarpDrive/master/wd.fish > ~/.config/fish/functions/wd.fish # downloads the newest fish function file
 curl -sS https://raw.githubusercontent.com/quackduck/WarpDrive/master/src/WarpDrive.java > "$wd_source_containing_dir"/WarpDrive.java # downloads the newest java source code file
 cd $wd_source_containing_dir && javac WarpDrive.java && rm WarpDrive.java && cd - # compiles the java file and then deletes it
-curl -sS https://raw.githubusercontent.com/quackduck/WarpDrive/master/man/man1/wd.1 > /usr/local/share/man/man1/wd.1 # downloads man page
+
+# Get man page
+if test (uname) = "Darwin"
+  mkdir -p /usr/local/share/man/man1
+  curl -sS https://raw.githubusercontent.com/quackduck/WarpDrive/master/man/man1/wd.1 > /usr/local/share/man/man1/wd.1 # downloads man page
+else
+  echo "Your password is needed to install the man page for WarpDrive"
+  sudo mkdir -p /usr/local/share/man/man1
+  curl -sS https://raw.githubusercontent.com/quackduck/WarpDrive/master/man/man1/wd.1 | sudo tee /usr/local/share/man/man1/wd.1 > /dev/null # downloads man page
+end
