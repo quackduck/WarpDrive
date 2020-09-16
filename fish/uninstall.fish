54b72595e72b9fe3d8ea4d5ec7ba1857b8bb0d83
Ishan Goel <igoel.mail@gmail.com>
2020-09-16 07:53:33 +0400

    Remove ~/.WarpDrive and not just ~/.WarpDrive/WarpDrive.class when uninstalling

--- fish/uninstall.fish
+++ fish/uninstall.fish
@@ -1,5 +1,5 @@
 echo "Uninstalling WarpDrive fully. Goodbye! Report bugs or issues at https://github.com/quackduck/WarpDrive/issues"
-rm ~/.config/fish/conf.d/wd_on_prompt.fish ~/.config/fish/functions/wd.fish ~/.WarpDrive/WarpDrive.class
+rm -r ~/.config/fish/conf.d/wd_on_prompt.fish ~/.config/fish/functions/wd.fish ~/.WarpDrive
 if test (uname) = "Darwin"
     rm /usr/local/share/man/man1/wd.1 /usr/local/share/fish/vendor_completions.d/wd.fish
 else
